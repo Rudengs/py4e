cc4744579dbb5a92f41505e8607a13d622df7c50
Rudengs <rudengkim@gmail.com>
2019-04-07 13:42:44 +0900

    Chapter 2

---
+++ exercise/ex_02.py
@@ -0,0 +1,7 @@
+name = input('Enter your name: ')
+print('hello', name)
+
+hours = input('Enter Hours: ')
+rate = input('Enter Rate: ')
+pay = float(hours) * float(rate)
+print(name, 'your pay is', pay)
